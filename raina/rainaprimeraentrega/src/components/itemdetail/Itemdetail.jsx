3e08680330a320c2472f19f6744e6b708a0da8c5
Irina vilas <irinavilas@hotmail.com>
2023-11-14 21:11:50 -0300

    final segunda pre entrega

---
+++ raina/rainaprimeraentrega/src/components/itemdetail/Itemdetail.jsx
@@ -0,0 +1,22 @@
+import { ItemCount } from "../Itemcount"
+
+export const ItemDetail = ( {description, price, stock, name, } ) => {
+
+  const onAdd = (items) => { 
+    alert(`Se agregaron ${items} al carrito`)
+ }
+
+  return (
+    <div className="border m-2">
+        <div className="card ">
+        <div className="card-body ">
+          <h5 className="card-title">{name}</h5>
+          <img src={img} alt="" />
+          <p className="card-text"> {description} </p>
+          <p>Precio: {price} </p>
+          <ItemCount stock={stock} onAdd={onAdd} />
+        </div>
+      </div>
+    </div>
+  )
+}
