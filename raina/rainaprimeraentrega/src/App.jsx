47d1bfee2ecf5902336861707e68ab4e1ceee767
Irina vilas <irinavilas@hotmail.com>
2023-11-21 17:46:29 -0300

    saque app de carpeta

--- raina/rainaprimeraentrega/src/App.jsx
+++ raina/rainaprimeraentrega/src/App.jsx
@@ -1,5 +1,5 @@
-import { ItemListContainer } from "../components/ItemListContainer/ItemListContainer";
-import { NavBar } from "../components/NavBar/NavBar";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import { NavBar } from "./components/NavBar/NavBar";
 
 const productos = [
   {
